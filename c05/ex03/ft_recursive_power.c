350258b27a3896199decc302c6b3ea32573753ad
Andrey Slyusar <a97k572@gmail.com>
2024-06-20 14:14:05 +0300

    Exercise 03: ft_recursive_power
    ft_recursive_power
    
    Signed-off-by: Andrey Slyusar <a97k572@gmail.com>

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhelena <fhelena@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 14:06:23 by fhelena           #+#    #+#             */
+/*   Updated: 2024/06/20 14:13:32 by fhelena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	res;
+
+	res = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	else if (power == 0)
+	{
+		return (1);
+	}
+	else if (power > 1)
+	{
+		res *= ft_recursive_power(nb, power - 1);
+	}
+	else
+	{
+		return (nb);
+	}
+	return (res);
+}
