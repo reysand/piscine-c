a893876fd31b5c8c0e72a3050af7d0941fea6959
Andrey Slyusar <a97k572@gmail.com>
2024-06-25 13:04:51 +0300

    Exercise 02: ft_rev_params
    ft_rev_params
    
    Signed-off-by: Andrey Slyusar <a97k572@gmail.com>

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhelena <fhelena@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 13:03:16 by fhelena           #+#    #+#             */
+/*   Updated: 2024/06/25 13:04:26 by fhelena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str++, 1);
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		ft_putstr(argv[i]);
+		--i;
+	}
+	return (0);
+}
