203d8cd247849c91507d435201ea3206b71c2346
Andrey Slyusar <a97k572@gmail.com>
2024-06-25 12:56:22 +0300

    Exercise 00: ft_print_program_name
    ft_print_program_name
    
    Signed-off-by: Andrey Slyusar <a97k572@gmail.com>

---
+++ c06/ex00/ft_print_program_name.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhelena <fhelena@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 12:51:38 by fhelena           #+#    #+#             */
+/*   Updated: 2024/06/25 12:59:19 by fhelena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str++, 1);
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	ft_putstr(argv[argc - argc]);
+	return (0);
+}
