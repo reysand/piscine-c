d869a8e4a00144d5070f416f1171e1029f8eaf50
Andrey Slyusar <a97k572@gmail.com>
2024-06-25 13:01:30 +0300

    Exercise 01: ft_print_params
    ft_print_params
    
    Signed-off-by: Andrey Slyusar <a97k572@gmail.com>

---
+++ c06/ex01/ft_print_params.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhelena <fhelena@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 12:57:48 by fhelena           #+#    #+#             */
+/*   Updated: 2024/06/25 13:01:16 by fhelena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str++, 1);
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		++i;
+	}
+	return (0);
+}
